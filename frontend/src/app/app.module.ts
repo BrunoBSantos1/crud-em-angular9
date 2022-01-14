7031d52c71ffcba69cfb721e40826ac79ee64aad
Bruno Batista <bruno.batista@inventione.com.br>
2022-01-13 21:11:00 -0300

    Criado as diretivas Extrutural

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,8 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
