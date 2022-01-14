00bee4bd1be0422459ef40f89b70e8a6ef647cc9
Bruno Batista <bruno.batista@inventione.com.br>
2022-01-13 22:44:19 -0300

    Criado o service do produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,7 +40,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private producService: ProductService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.producService.showMessage('Produto criado com sucesso')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
