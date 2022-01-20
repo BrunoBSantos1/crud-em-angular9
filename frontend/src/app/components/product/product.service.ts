80efa93abb9190368afe7d079eef902465d63eec
Bruno Batista <bruno.batista@inventione.com.br>
2022-01-20 10:52:25 -0300

    Criado metodo para fazer atualização de um produto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -33,4 +33,9 @@ export class ProductService {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<Product>(url)
   }
+
+  update(product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
 }
