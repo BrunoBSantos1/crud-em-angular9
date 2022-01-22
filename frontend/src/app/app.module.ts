aa6fdff01d44d0ae5e59bc44610049bed176c9dc
Bruno Batista <bruno.batista@inventione.com.br>
2022-01-21 22:55:50 -0300

    Criado a função de atualizar produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -47,7 +48,8 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
