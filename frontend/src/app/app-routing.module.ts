4be16a32224d57ba0cafd9e383a9eb42fe59eb75
Bruno Batista <bruno.batista@inventione.com.br>
2022-01-24 19:19:13 -0300

    Criado rota para componente de exclusão do produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -21,6 +22,10 @@ const routes: Routes = [{
 {
   path: "products/update/:id",
   component: ProductUpdateComponent
+},
+{
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
 }
 ];
 
